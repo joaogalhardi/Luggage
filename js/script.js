24827c73c1ea926968c238c247d31580951196ab
Joao Galhardi <joaogalhardi.dev@gmail.com>
2022-04-06 10:26:50 -0300

    header + menu mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const btnMobile = document.querySelector("#mobile-menu");
 const nav = document.querySelector("nav");
+const links = document.querySelectorAll('.menu li')
 
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('active')
+  })
+})
 
 function toggleMenu(){
   nav.classList.toggle("active");
